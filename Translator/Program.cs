beac0965b8d7c7882419b8c5c6154ab61da216e2
nad436 <www.tanya0197@gmail.com>
2024-08-07 19:39:28 +0200

    first build

--- Translator/Program.cs
+++ Translator/Program.cs
@@ -10,6 +10,36 @@ namespace Translator
     {
         static void Main(string[] args)
         {
+           
+            Console.WriteLine($"Hello!\nThis is a translator.\nPlease write an English word to get its translation in Ukrainian");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            switch (word) 
+            {
+                case "hello":
+                    Console.WriteLine("привіт");
+                    break;
+                case "goodbye":
+                    Console.WriteLine("допобачення");
+                    break;
+                case "thanks":
+                    Console.WriteLine("дякую");
+                    break;
+                case "programming":
+                    Console.WriteLine("програмування");
+                    break;
+                case "silence":
+                    Console.WriteLine("мовчання");
+                    break;
+                case "lamb":
+                    Console.WriteLine("ягня");
+                    break;
+                default:
+                    Console.WriteLine("Sorry, but you can't translate this");
+                    break;
+                    
+            }
+        }
         }
     }
-}
+
